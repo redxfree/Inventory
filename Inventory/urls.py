bc620e3482a687d0fef8a88dd1e28d3fa54ce70b
redxfree <100827526+redxfree@users.noreply.github.com>
2022-08-03 15:13:46 +0530

    hello

--- Inventory/urls.py
+++ Inventory/urls.py
@@ -18,13 +18,16 @@ from django.urls import path,include
 from rest_framework_simplejwt import views as jwt_views
 from django.conf import settings
 from django.conf.urls.static import static
-
+from django.views.static import serve
+from django.conf.urls import url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('book.urls')),
      path('api/token/',jwt_views.TokenObtainPairView.as_view(),name= 'token_obtain_pair'),
     path('api/token/refresh',jwt_views.TokenRefreshView.as_view(),name= 'token_refresh'),    
-  
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+   url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
+
+]
     
