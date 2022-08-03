00e4fba8093860308f6c44c68dfc7451f25ad2fc
redxfree <100827526+redxfree@users.noreply.github.com>
2022-08-03 15:31:01 +0530

    gnv

--- Inventory/settings.py
+++ Inventory/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'o9ml=qgrqcvn+*9u6i++*dwp-=n_6c7-o%zbwgp8_gvab(_6!%'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1','https://booksinvent.herokuapp.com/']
+ALLOWED_HOSTS = ['127.0.0.1:8000','booksinvent.herokuapp.com']
 
 
 # Application definition
